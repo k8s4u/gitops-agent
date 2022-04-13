375942a28ae8e63767c33dcef16dec4d7d84e2ff
Olli Janatuinen <olljanat@users.noreply.github.com>
2022-04-13 13:26:49 +0200

    Use PAT authentication (#1)
    * Use PAT instead of SSH
    
    * Use authentication header on GitHub compatible format
    
    * Do not wrap PAT token, use HTTP/1.1

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
-cd /gitops
-if [ ! -d "/gitops/.git" ]; then
-  git clone $GITREPO .
+
+# Create subfolder because /gitops is owned by root and git complains about it
+if [ ! -d "/gitops/code" ]; then
+  mkdir /gitops/code
 fi
-git pull
+
+# Get personal access token as base64 encoded string
+cd /gitops/code
+PAT=$(cat /git-auth/pat)
+B64_PAT=$(printf "%s""x-access-token:$PAT" | base64 -w 0)
+
+# Initial clone
+if [ ! -d "/gitops/code/.git" ]; then
+  git -c http.extraHeader="Authorization: Basic ${B64_PAT}" -c http.version="HTTP/1.1" clone $GITREPO .
+fi
+
+git -c http.extraHeader="Authorization: Basic ${B64_PAT}" -c http.version="HTTP/1.1" pull
+
 kubectl apply -k envs/$ENVIRONMENT
 exit $?
